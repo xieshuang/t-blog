1c49415ed574d5228018a5c3413648bacc978f8a
xieshuang <xieshuang@broadtext.com.cn>
2018-03-22 10:54:31 +0800

    config mybatis

--- src/main/java/com/xsh/blog/BlogApplication.java
+++ src/main/java/com/xsh/blog/BlogApplication.java
@@ -1,13 +1,18 @@
 package com.xsh.blog;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import javax.sql.DataSource;
 
+@MapperScan("com.xsh.blog.dao")
 @SpringBootApplication
 public class BlogApplication {
 
@@ -15,6 +20,15 @@ public class BlogApplication {
     @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource dataSource() {
         return new DruidDataSource();
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactoryBean() throws Exception {
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource());
+        sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath*:/mapper/*Mapper.xml"));
+        return sqlSessionFactoryBean.getObject();
     }
     
     public static void main(String[] args) {
