83734906f71560986a599e8e954e256f2859e6cb
xieshuang <xieshuang@broadtext.com.cn>
2018-03-22 10:43:20 +0800

    config datasource

--- src/main/java/com/xsh/blog/BlogApplication.java
+++ src/main/java/com/xsh/blog/BlogApplication.java
@@ -1,12 +1,23 @@
 package com.xsh.blog;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class BlogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BlogApplication.class, args);
-	}
+    @Bean(initMethod = "init", destroyMethod = "close")
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return new DruidDataSource();
+    }
+    
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
 }
